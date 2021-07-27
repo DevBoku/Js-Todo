d908b286d9c7899ff6d08ebf189aeef228393fef
woooWha <gilga4@naver.com>
2021-07-27 17:15:10 +0900

    todoListNotMVC

--- app.js
+++ app.js
@@ -68,27 +68,21 @@ function paintTodolist(text){
 function handleSubmitList(event){
     event.preventDefault();
     const currentValue = todoInput.value;
-    saveTodolist(currentValue);
     paintTodolist(currentValue);
     todoInput.value="";
 }
 
-function todoAll(){
-    alert('안녕')
-}
 
-
-
-const TODOLIST = "TODOLIST"
-
-function loadtodo(){
-    const todoListValue = localStorage.getItem(TODOLIST);
-    if(todoListValue !==null){
-        const parsedtodoList = JSON.parse(todoListValue);
-        parsedtodoList.forEach(function(todo){
-            paintTodolist(todo.text);
-        });
-    }
+//리스트 분류 부분(comple와 active는 합칠 수 있지만... 그게 문제가 아니다....)
+function completeList(){
+    checkedList = document.querySelectorAll("input[name=todoCheckBox]");
+    filtered = checkedList.forEach(todo => {if(todo.checked === false){
+        const todoParent = todo.parentNode;
+        todoParent.style.display = 'none';
+    }else if(todo.checked===true){
+        const todoParent = todo.parentNode;
+        todoParent.style.display = 'block';
+    }})
 }
 
 function activeList(){
@@ -102,28 +96,22 @@ function activeList(){
     }})
 }
 
-function completeList(){
-    checkedList = document.querySelectorAll("input[name=todoCheckBox]");
-    filtered = checkedList.forEach(todo => {if(todo.checked === false){
-        const todoParent = todo.parentNode;
-        todoParent.style.display = 'none';
-    }else if(todo.checked===true){
-        const todoParent = todo.parentNode;
-        todoParent.style.display = 'block';
-    }})
-}
-
 function allList(){
     checkedList = document.querySelectorAll("input[name=todoCheckBox]");
-    console.log(checkedList)
     filtered = checkedList.forEach(todo => todo.parentNode.style.display = 'block')
 }
 
 function clearList(){
     checkedList = document.querySelectorAll("input[name=todoCheckBox]");
     filtered = checkedList.forEach(todo => {if(todo.checked === true){
-        const todoParent = todo.parentNode;
-        todoParent.parentNode.removeChild(todoParent);
+        const div = todo.parentNode;
+        const li = div.parentNode;
+        todoUl.removeChild(li);
+        const cleanTask = todolist.filter(function(todo){
+            return todo.id !== parseInt(li.id);
+        });
+        todolist=cleanTask;
+        saveTodolist();
     }})
 }
 
@@ -135,6 +123,20 @@ function allCheckList(){
         todo.parentNode.parentNode.classList.add('completed')}
         );
 }
+
+//새로고침 불러오기
+const TODOLIST = "TODOLIST"
+function loadtodo(){
+    const todoListValue = localStorage.getItem(TODOLIST);
+    if(todoListValue !==null){
+        const parsedtodoList = JSON.parse(todoListValue);
+        parsedtodoList.forEach(function(todo){
+            paintTodolist(todo.text);
+        });
+    }
+}
+
+
 
 function init(){
     loadtodo();
